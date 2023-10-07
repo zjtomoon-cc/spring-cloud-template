79a07d8602b4c2cb5b139f7696754fe325ca8c85
lastnight2022 <zjtomoon@dingtalk.com>
2023-10-07 23:17:03 +0800

    init project

---
+++ src/main/java/com/zjtomooncc/springcloudtemplate/SpringCloudTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.zjtomooncc.springcloudtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zjtomooncc/springcloudtemplate/SpringCloudTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjtomooncc.springcloudtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
